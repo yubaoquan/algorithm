3ea5bf7a450ba865493d8242290358122b183e25
yubaoquan <721731428@qq.com>
2014-10-11 18:30:39 +0800

    1-80全部AC的梦想貌似破灭了
    有的题目用java就是超时;
    目前没有办法;

--- src/acm_club/_51/P1057.java
+++ src/acm_club/_51/P1057.java
@@ -1,61 +1,67 @@
 package acm_club._51;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author	yubaoquan
+ * @Date	2014年10月11日
+ * @Description	超时
+ *
+ */
 public class P1057 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNext()) {
-			System.out.println(foo(sc.nextLine()));
+			System.out.println(findHuiWen(sc.nextLine()));
 		}
 		sc.close();
-//		System.out.println(isHuiWen("".toCharArray()));
-//		System.out.println(isHuiWen("1".toCharArray()));
-//		System.out.println(isHuiWen("12".toCharArray()));
-//		System.out.println(isHuiWen("123".toCharArray()));
-//		System.out.println(isHuiWen("12321".toCharArray()));
-//		System.out.println(isHuiWen("1231321".toCharArray()));
 	}
 
-	private static char[] foo(String input) {
-		char[] array = new char[input.length() + 1];
-		array[0] = '0';
-		for (int i = 1;i < array.length;i++) {
-			array[i] = input.charAt(i);
+	private static char[] findHuiWen(String input) {
+		boolean everyNumberIsNine = true;
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) != '9') {
+				everyNumberIsNine = false;
+				break;
+			}
 		}
+		if (everyNumberIsNine) {
+			char[] result = new char[input.length() + 1];
+			Arrays.fill(result, '0');
+			result[0] = '1';
+			result[result.length - 1] = '1';
+			return result;
+		}
+		char[] array = input.toCharArray();
 		while (true) {
+			increase(array);
 			if (isHuiWen(array)) {
 				return array;
 			}
-			increase(array);
 		}
 	}
 
 	private static boolean isHuiWen(char[] array) {
-		for (int i = 0;i < array.length / 2;i++) {
+		for (int i = 0; i < array.length / 2; i++) {
 			if (array[i] != array[array.length - 1 - i]) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	private static void increase(char[] array) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private static boolean isHuiWen(String input) {
-		char[] array = new char[input.length()];
-		for (int i = 0; i < input.length(); i++) {
-			array[i] = input.charAt(input.length() - i - 1);
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (array[i] == '9') {
+				array[i] = '0';
+			} else {
+				array[i]++;
+				return;
+			}
 		}
-		if (new String(array).equals(input)) {
-			return true;
-		}
-		return false;
 	}
+
 }
---
+++ src/acm_club/_51/P1057_2.java
@@ -0,0 +1,53 @@
+package acm_club._51;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P1057_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i, tail, middleRight, flag;
+		while (sc.hasNext()) {
+			char[] array = sc.nextLine().toCharArray();
+			boolean allNine = true;
+			for (int index = 0; index < array.length; index++) {
+				if (array[index] != '9') {
+					allNine = false;
+					break;
+				}
+			}
+			if (allNine) {
+				char[] result = new char[array.length + 1];
+				Arrays.fill(result, '0');
+				result[0] = '1';
+				result[result.length - 1] = '1';
+				System.out.println(result);
+				continue;
+			}
+			tail = array.length - 1;
+			middleRight = (tail + 1) >> 1;
+
+			for (i = middleRight - 1, flag = 0; i >= 0 && flag == 0; i--) {
+				flag = array[i] == array[tail - i] ? 0 : (array[i] > array[tail - i] ? 1
+						: -1);
+			}
+
+			if (flag <= 0) {
+				array[tail >> 1]++;
+				for (i = tail >> 1; i != 0 && array[i] > '9'; i--) {
+					array[i] = '0';
+					array[i - 1]++;
+				}
+			}
+
+			for (i = 0; i < middleRight; i++)
+				System.out.print(array[i]);
+			for (i = middleRight - (tail & 1); i >= 0; i--)
+				System.out.print(array[i]);
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
