072510b6964d56e6cf027645eb6ff9d8fdc5817f
yubaoquan <1079139460@qq.com>
2014-10-22 23:06:37 +0800

    写的很难看;

--- src/acm_club/_1100/P1132.java
+++ src/acm_club/_1100/P1132.java
@@ -1,45 +1,83 @@
 package acm_club._1100;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1132 {
 
+	private static int[][] book;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNext()) {
-			String[] input = sc.nextLine().split(" ");
-			System.out.println(longestCommonSubsequence(input[0], input[1], input, 0));
+			String[] input = sc.nextLine().split(" +");
+			book = new int[input[0].length()][input[1].length()];
+			for (int i = 0; i < input[0].length(); i++) {
+				Arrays.fill(book[i], -1);
+			}
+			System.out.println(longestCommonSubsequence(input[0], input[1],
+					input[0].length() - 1, input[1].length() - 1));
 		}
 		sc.close();
 	}
 
 	private static int longestCommonSubsequence(String string1, String string2,
-			int shortIndex, int longIndex) {
-		// TODO Auto-generated method stub
+			int index1, int index2) {
 		int result = 0;
-		String shortStr = (string1.length() < string2.length()) ? string1 : string2;
-		String longStr = (string1.length() >= string2.length()) ? string1 : string2;
-		int i = shortIndex ;
-		int j = longIndex ;
-		if (i + j== 0) {
-			return 0;
-		}
-		if (i == 1) {
-			if (longStr.contains(shortStr.subSequence(0, 1))) {
+		if (index1 == 0 && index2 == 0) {
+			if (string1.charAt(index1) == string2.charAt(index2)) {
 				return 1;
 			}
 			return 0;
-		}
-		
-		for (; i > 0 && j > 0;) {
-			if (shortStr.charAt(i) == longStr.charAt(j)) {
-				i--;
-				result++;
+		} else if(index1 == 0) {
+			while (index2!=-1) {
+				if (string1.charAt(index1) == string2.charAt(index2)) {
+					return 1;
+				} else {
+					index2--;
+					if (index2 == -1) {
+						return 0;
+					}
+				}
 			}
-			j--;
-			result += longestCommonSubsequence(string1, string2, i, j);
+		}else if (index2 == 0) {
+			while (index1!=-1) {
+				if (string1.charAt(index1) == string2.charAt(index2)) {
+					return 1;
+				} else {
+					index1--;
+					if (index1==-1) {
+						return 0;
+					}
+				}
+			}
+		}
+		if (string1.charAt(index1) == string2.charAt(index2)) {
+			int a = book[index1 - 1][index2 - 1];
+			if (a == -1) {
+				a = longestCommonSubsequence(string1, string2, index1 - 1,
+						index2 - 1);
+				book[index1 - 1][index2 - 1] = a;
+			}
+			result = 1 + a;
+
+		} else {
+			int a = book[index1 - 1][index2];
+			int b = book[index1][index2 - 1];
+			if (a == -1) {
+				a = longestCommonSubsequence(string1, string2, index1 - 1,
+						index2);
+				book[index1 - 1][index2] = a;
+			}
+			if (b == -1) {
+				b = longestCommonSubsequence(string1, string2, index1,
+						index2 - 1);
+				book[index1][index2 - 1] = b;
+			}
+			result = Math.max(a, b);
 		}
 		return result;
 	}
+
 
 }
